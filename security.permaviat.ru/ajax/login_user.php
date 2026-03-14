bf8e403ae9b71f0f81ad83ddf5cef6c1bdfa4f33
Anecka7L <165366413+Anecka7L@users.noreply.github.com>
2026-03-14 21:56:02 +0500

    Update login_user.php

--- security.permaviat.ru/ajax/login_user.php
+++ security.permaviat.ru/ajax/login_user.php
@@ -1,9 +1,25 @@
 <?php
 	session_start();
-	include("../settings/connect_datebase.php");
+	require_once("../settings/connect_datebase.php");
+	require_once("../libs/autoload.php");
 	
 	$login = $_POST['login'];
 	$password = $_POST['password'];
+	
+
+	if(isset($_POST["g-recaptcha-response"]) == false){
+		echo "Нет пройденной \"Я не робот\"";
+		exit;
+	}
+	
+	$Secret = "6Lc37YYsAAAAAJCnHMhwAZEE3spSqFEJvOIC18V2";
+	$Recaptcha = new \ReCaptcha\ReCaptcha($Secret);
+	$Response = $Recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER['REMOTE_ADDR']);
+
+	if(!$Response->isSuccess()){
+		echo "Пользователь не распознан.";
+		exit;
+	}
 	
 	// ищем пользователя
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
